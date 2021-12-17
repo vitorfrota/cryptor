bfe1657e24809da0fbb04154e5a4d6ac18ab3a39
Vitor <vitorfrota@live.com>
2021-12-16 22:43:27 -0400

    feature: create home page with styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,33 @@
-
+import './styles.scss';
 
 function Home(){
     return (
-        <div><h1>Home</h1></div>
+        <main className='wrapper'>
+            <header>
+                <h1>Cryptor<strong>.</strong></h1>
+                <p>Encrypt/Decrypt your text with a password.</p>
+            </header>
+            <div className='box'>
+                <ul>
+                    <li>Encrypt</li>
+                    <li>Decrypt</li>
+                </ul>
+                <textarea
+                    placeholder="Put your text to encrypt here..."
+                    rows={4}
+                />
+                <div className="boxFooterContainer">
+                    <div className="keyContainer">
+                        <input 
+                            type="text" 
+                            placeholder="Put your secret key here..."
+                        />
+                    </div>
+                    <button>Encrypt</button>
+                </div>
+            </div>
+            <footer>Created by Vitor Frota.</footer>
+        </main>
     )
 }
 

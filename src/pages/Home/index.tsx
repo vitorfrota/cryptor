f210603198531246db0e2f9ebdc127980be7626d
Vitor <vitorfrota@live.com>
2021-12-16 23:05:37 -0400

    feature: add states to update selected option

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from 'react';
+import { FiKey } from 'react-icons/fi';
 import './styles.scss';
 
 function Home(){
+    const [selectedOption, setSelectedOption] = useState('encrypt');
+    const [canConvert, setCanConvert] = useState(false);
+    const [text, setText] = useState('');
+    const [key, setKey] = useState('');
+
+    useEffect(()=> {
+        if(text !== '' && key !== ''){
+            setCanConvert(true)
+        }else {
+            setCanConvert(false);
+        }
+    }, [key, text, canConvert]);
+
+    function handleChangeOption(){
+        setText('');
+
+        selectedOption === 'encrypt' 
+        ? setSelectedOption('decrypt') 
+        : setSelectedOption('encrypt')
+    }
     return (
         <main className='wrapper'>
             <header>
@@ -9,21 +31,32 @@ function Home(){
             </header>
             <div className='box'>
                 <ul>
-                    <li>Encrypt</li>
-                    <li>Decrypt</li>
+                    <li 
+                        className={selectedOption === 'encrypt' ? 'active' : ''}
+                        onClick={handleChangeOption}
+                    >Encrypt</li>
+                    <li 
+                        className={selectedOption === 'decrypt' ? 'active' : ''}
+                        onClick={handleChangeOption}
+                        >Decrypt</li>
                 </ul>
                 <textarea
-                    placeholder="Put your text to encrypt here..."
+                    placeholder={`Put your text to ${selectedOption} here...`}
+                    onChange={e => setText(e.target.value)}
+                    value={text}
                     rows={4}
                 />
                 <div className="boxFooterContainer">
                     <div className="keyContainer">
+                        <FiKey />
                         <input 
                             type="text" 
                             placeholder="Put your secret key here..."
+                            onChange={e => setKey(e.target.value)}
+                            value={key}
                         />
                     </div>
-                    <button>Encrypt</button>
+                    <button disabled={!canConvert}>{selectedOption}</button>
                 </div>
             </div>
             <footer>Created by Vitor Frota.</footer>
